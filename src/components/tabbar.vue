508681af3aec47b766f907c083bd18feea230841
chiewm <zmhbest@sina.cn>
2018-03-02 22:35:37 +0800

    update

--- src/components/tabbar.vue
+++ src/components/tabbar.vue
@@ -4,25 +4,28 @@
     </div>
 </template>
 <script>
-    import mTabbarItem from './tabbar-item';
-    export default {
-        props: ['value']
+import mTabbarItem from "./tabbar-item";
+export default {
+  props: ["value"],
+  computed: {
+    values() {
+      return this.$route.matched[0].name;
     }
+  }
+};
 </script>
 <style lang="less">
-.m-tabbar{
-    display: flex;
-    flex-direction: row;
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    width: 100%;
-    overflow: hidden;
-    height: 50px;
-    background: #fff;
-    border-top: 1px solid #e4e4e4;
+.m-tabbar {
+  display: flex;
+  flex-direction: row;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  overflow: hidden;
+  height: 50px;
+  background: #fff;
+  border-top: 1px solid #e4e4e4;
 }
-
-
 </style>
---
+++ src/pages/Index/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <m-header title="豆瓣app" fixed :bg="true">
+      <a href="javascript:;" slot="right"><img class="m-icon-img margin-right-10" src="../../assets/images/ic_actionbar_search_icon.png"/></a>
+      <a href="javascript:;" slot="right"><img class="m-icon-img" src="../../assets/images/ic_chat_green.png"/></a>
+    </m-header>
+  </div>
+</template>
+
+<script>
+import mTabbar from "../../components/tabbar";
+import mTabbarItem from "../../components/tabbar-item";
+import mHeader from "../../components/header"
+
+export default {
+  name: "index",
+  components: {
+    mTabbar,
+    mTabbarItem,
+    mHeader
+  },
+  data() {
+    return {
+      select: "tab1"
+    };
+  }
+};
+</script>
